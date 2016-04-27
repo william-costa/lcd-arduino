d0ec2c86a3ba3cf7c9935f9225d45e5abb034064
William Costa <william@webart.com.br>
2016-04-27 13:46:44 -0300

    Inicio da Tela LCD

--- lcd.php
+++ lcd.php
@@ -1,3 +1,12 @@
+<?php
+
+$lcd = array();
+
+$lcd['linhas']  = 2;
+$lcd['colunas'] = 16;
+
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -15,9 +24,34 @@
 </head>
 
 <body>
-
+	<h1>LCD Arduino</h1>
 <div class="lcd">
-</div>
 
+	<?php
+
+
+
+	for($w = 0; $w<$lcd['linhas']; $w++){
+
+		echo '<div class="linha">';
+
+			for($k = 0; $k<$lcd['colunas']; $k++){
+				echo '<div class="digito">';
+				for($i = 0; $i < 8; $i++){
+					echo '<canvas id="pixel-0-0" class="pixel off"></canvas>
+								<canvas id="pixel-0-1" class="pixel off"></canvas>
+								<canvas id="pixel-0-2" class="pixel off"></canvas>
+								<canvas id="pixel-0-3" class="pixel off"></canvas>
+								<canvas id="pixel-0-4" class="pixel off"></canvas> ';
+				}
+				echo '</div>';
+			}
+
+		echo '</div>';
+
+	}
+
+	?>
+</div>
 </body>
 </html>
